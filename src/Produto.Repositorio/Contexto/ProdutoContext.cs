55564d91032be8f740e9cd053ef3d3d2cdc3b7c0
Anderson azola <andersonazola492@gmail.com>
2026-07-12 12:54:35 -0300

    chore: configuracoes do banco e migrations

--- src/Produto.Repositorio/Contexto/ProdutoContext.cs
+++ src/Produto.Repositorio/Contexto/ProdutoContext.cs
@@ -8,7 +8,8 @@ public class ProdutoContext : DbContext
 {
     public DbSet<Produtos> Produtos { get; set; }
 
-    public ProdutoContext() { }
+    public ProdutoContext(DbContextOptions<ProdutoContext> options) : base(options)
+    { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
